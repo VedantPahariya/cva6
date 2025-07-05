a6fc10770a7d48ae6fbd0ccc368b0d35e4d28839
VedantPahariya <vedant.pahariya@research.iiit.ac.in>
2025-07-05 23:19:37 +0530

    Added personal Tests

---
+++ verif/tests/custom/mytest/multiply.c
@@ -0,0 +1,11 @@
+// #include <stdint.h>
+// #include <stdio.h>
+
+int main() {
+    int a = 21;
+    int b = 71;
+    int result = a * b;
+    // printf("The result of multiplying ");
+    // Optionally, you can return the result or just return 0
+    return 0;
+}
